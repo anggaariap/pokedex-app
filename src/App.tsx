09484f6ce9ba7a43b1b2c0fc9c652b832560c8e1
anggaariap <anggaariap@gmail.com>
2024-07-11 09:02:04 +0700

    add path for pokemonDetil

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import HomePage from "./pages/homePage"
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import CssBaseline from "@mui/material/CssBaseline";
+import PokemonDetil from "./components/pokemonDetil";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "pokemon/:pokemonName",
+    element: <PokemonDetil/>
+  }
 ]);
 
 function App() {
